523461fed709fc5bd7280ee0adef3bdab4b4b126
omerek1 <32849222+omerek1@users.noreply.github.com>
2023-01-08 09:23:59 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import random # to import machine values randomly
+import random 
 
-MAX_LINES = 3 # All caps bcz its a constant value (doesnt change) (this is a convention u do in all caps in python) 
+MAX_LINES = 3 
 MAX_BET = 100
-MIN_BET = 1   # anywhere in the program when u ref nmber of lines we will write max lines instead of 3
+MIN_BET = 1   
 
 ROWS = 3
 COLS = 3
@@ -24,10 +24,10 @@ symbol_value = {
 def check_winnings(columns, lines, bet, values):
     winnings = 0
     winning_lines = []
-    for line in range(lines): # 1 bet = line =0, lines = 1 / 2 bet = line 0,1 lines = 2 / line = 0,1,2 lines = 3 (looping through every row)
-        symbol = columns[0][line] # we have all the columns so we use columns (symbol that we wanna check is whatever symbol is in the first column of the current row)
-        for column in columns: # now we know the symbol we want to check, so we loop through every single column
-            symbol_to_check = column[line] #
+    for line in range(lines):
+        symbol = columns[0][line] 
+        for column in columns:
+            symbol_to_check = column[line] 
             if symbol != symbol_to_check: # if not the same, break (and check other lines if there are any)
                 break
         else:       # if they are the same they win, and the else statement runs
@@ -40,48 +40,48 @@ def check_winnings(columns, lines, bet, values):
 def get_slot_machine_spin(rows, cols, symbols): # Generate slot machine items
     all_symbols = []
     for symbol, symbol_count in symbols.items(): # .items gives u both the key(symbol) and value (symbol_counter)
-        for _ in range(symbol_count): #looping through the symbol count _ = anonomus value
+        for _ in range(symbol_count): #looping through the symbol count = anonomus value
             all_symbols.append(symbol)
 
-    columns = []                    #each nested list represents a column value / define column
+    columns = []                    
     for _ in range(cols):                 # generate a column for every single column we have
-        column = []                     # all this code below picks a random value
+        column = []                     
         current_symbols = all_symbols[:]            # [:] slice operator, current symbols(the one we can choose from) is a copy of all symbols
-        for _ in range(rows):                 # loop through the amount of rows we have to gen
-            value = random.choice(current_symbols)                # first value is a random choice of current symbols
-            current_symbols.remove(value)                   # remove the value so we dont pick it again
-            column.append(value)                    # then we add it to the column
+        for _ in range(rows):                 
+            value = random.choice(current_symbols)             
+            current_symbols.remove(value)                   
+            column.append(value)                 
 
         columns.append(column)
 
     return columns
 
 def print_slot_machine(columns):
-    for row in range(len(columns[0])): # assumes we have 1 += column, if we did pass smth that had no column it crashes
-        for i, column in enumerate(columns): # enumerate = gives index aswell the item
-            if i != len(columns) -1: # is the max index to access in the column list (3 columns is 3, their index is 2)
-                print(column[row], end=" | ") # default of end is \n (moves to the next line) after every since column,we only wanna do it after every since row so we add |3
+    for row in range(len(columns[0])): 
+        for i, column in enumerate(columns): 
+            if i != len(columns) -1: 
+                print(column[row], end=" | ") # default of end is \n (moves to the next line) after every since column,we only wanna do it after every since row so we add |
             else:
                 print(column[row], end="")
         print()
     
 def deposit():  # responsible for collecting user input that gets the deposit from the user
-    while True: # While loop, continuely asks for a deposit amount until they input a valid amount
+    while True: 
         amount = input("What would you like to deposit? $")
-        if amount.isdigit(): # if the number is a valid number (negatives not true) (we check if its a digit first in order to convert it to int)
-            amount = int(amount) # By default it comes out as a string, so we set it to an integer
-            if amount > 0:  # if the amount is bigger than 0, its a valid amount
-                break # then we break out and return amount (line 15)
+        if amount.isdigit():
+            amount = int(amount) 
+            if amount > 0:  
+                break 
             else: 
-                print("Amount must be greater than 0.") # otherwise we print this
+                print("Amount must be greater than 0.")
 
         else:
-            print("Please enter a number.") # if its not a number we print this
+            print("Please enter a number.")
 
     return amount
 
 def get_number_of_lines(): # we ask to pick a number 1-3
-    while True: # While loop, continuely asks for a deposit amount until they input a valid amount
+    while True: 
         lines = input("Enter the amount of lines to bet on (1-" + str(MAX_LINES) + ")? ") # convert to a str bcz 1 is a string, otherwise a problem would occur.
         if lines.isdigit():  
             lines = int(lines)  
